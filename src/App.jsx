a0883e19c4ffa2f1e759b3278fbced38ee2df101
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-03-19 16:52:59 -0400

    Poke API and GitHub images

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,6 @@ import './App.css'
 import Pokemon from './components/Pokemon';
 
 function App() {
-  const [currPokemonNum, setCurrentPokemonNum] = useState(Math.floor(Math.random() * 1010) + 1);
-
-  // Randomly generate new pokemon number between 1 and 1010
-  const generateNewPokemonNum = () => {
-    let newNum = Math.floor(Math.random() * 1010) + 1;
-    setCurrentPokemonNum(newNum);
-  }
 
   return (
     <div className="App">
@@ -17,13 +10,7 @@ function App() {
         <h1>Pokémon Gallery</h1>
         <h3>Discover Pokémon you never knew existed!</h3>
 
-        <Pokemon pokemonNum={currPokemonNum}/>
-        
-        <button type='button' 
-        title='Generate new Pokémon' 
-        onClick={generateNewPokemonNum}>
-          Discover!
-        </button>
+        <Pokemon />
       </div>
     </div>
   )
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,22 +3,50 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const Pokemon = () => {
+    let [currPokemonNum, setCurrentPokemonNum] = useState(0);
+    const [currPokemonInfo, setCurrentPokemonInfo] = useState('')
+    const [currPokemonImage, setCurrentPokemonImage] = useState('');
 
-    const getPokemonInfo = async () => {
-        try {
-          const url = `https://pokeapi.co/api/v2/pokemon/${currPokemonNum}`;
-    
-          axios.get(url).then(result => {
-            console.log(result.data);
-          })
-        } catch (e) {
-          console.log(e);
+    const generateNewPokemon = async () => {
+        // Randomly generate new pokemon number between 1 and 777
+        setCurrentPokemonNum(currPokemonNum = Math.floor(Math.random() * 777) + 1);
+
+        // Formatted number is padded with zeros (Ex. 7 -> 007, 85 -> 085, 775 -> 775)
+        let formattedNum = 0;
+
+        if(currPokemonNum < 10) {
+            formattedNum = '00' + currPokemonNum;
         }
-      }
-      
+
+        else if(currPokemonNum < 100 && currPokemonNum >= 10) {
+            formattedNum = '0' + currPokemonNum;
+        }
+
+        else {
+            formattedNum = currPokemonNum;
+        }
+
+        // Fetch data from PokeAPI version 2
+        const url = `https://pokeapi.co/api/v2/pokemon/${currPokemonNum}`;
+
+        const newPokemon  = await axios.get(url).then(result => {
+            setCurrentPokemonInfo(result.data);
+        });
+
+        // Take higher quality image from GitHub repository
+        setCurrentPokemonImage(`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/imagesHQ/${formattedNum}.png`)
+    }
+
     return (
         <div className='pokemon'>
-            
+            <h1>{currPokemonNum}</h1>
+            <img className='pokemon-current-img' src={currPokemonImage} height='400px' width='400px' />
+
+            <button type='button' 
+            title='Generate new Pokémon' 
+            onClick={generateNewPokemon}>
+                Discover!
+            </button>
         </div>
     )
 }
